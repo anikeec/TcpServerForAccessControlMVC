89142de06bedaec8ed07b5238bc4d5129102fe9c
apu <pasha_anik@ukr.net>
2018-09-19 20:13:33 +0300

    Refactoring

--- src/main/java/com/apu/TcpServerForAccessControlMVC/controller/IndexController.java
+++ src/main/java/com/apu/TcpServerForAccessControlMVC/controller/IndexController.java
@@ -9,10 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.apu.TcpServerForAccessControlDB.entity.AccessMessage;
 import com.apu.TcpServerForAccessControlDB.entity.Card;
 import com.apu.TcpServerForAccessControlDB.entity.Device;
-import com.apu.TcpServerForAccessControlDB.repository.AccessMessageRepository;
 import com.apu.TcpServerForAccessControlDB.repository.CardRepository;
 import com.apu.TcpServerForAccessControlDB.repository.DeviceRepository;
 
