310992d348044b0a5afd747849bd388d86438a4e
apu <pasha_anik@ukr.net>
2018-09-21 19:45:36 +0300

    Fix bug - it was wrong enter page

--- src/main/java/com/apu/TcpServerForAccessControlMVC/config/WebSecurityConfig.java
+++ src/main/java/com/apu/TcpServerForAccessControlMVC/config/WebSecurityConfig.java
@@ -25,7 +25,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         .antMatchers("/admin/**").hasAuthority("ADMIN").anyRequest()
         .authenticated().and().csrf().disable().formLogin()
         .loginPage("/login").failureUrl("/login?error=true")
-        .defaultSuccessUrl("/info")
+        .defaultSuccessUrl("/card/view")
         .usernameParameter("email")
         .passwordParameter("password")
         .and().logout()
