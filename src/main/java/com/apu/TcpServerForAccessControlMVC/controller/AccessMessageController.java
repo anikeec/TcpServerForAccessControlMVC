5bf531cbe5c527181b6a17fba5b5b8b6df10328d
apu <pasha_anik@ukr.net>
2018-10-12 17:00:55 +0300

    Begin add pagination for AccessMessage

--- src/main/java/com/apu/TcpServerForAccessControlMVC/controller/AccessMessageController.java
+++ src/main/java/com/apu/TcpServerForAccessControlMVC/controller/AccessMessageController.java
@@ -5,9 +5,14 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.hibernate.Hibernate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.apu.TcpServerForAccessControlDB.entity.AccessMessage;
@@ -20,9 +25,14 @@ public class AccessMessageController {
     private AccessMessageRepository accessMessageRepository;
     
     @GetMapping("/accessMessage/view")
-    public ModelAndView index(Principal principal) {
+    public ModelAndView index(Principal principal, @RequestParam(value = "page", required = false) Integer page) {
         Map<String, Object> model = new HashMap<>();
-        List<AccessMessage> accessMessageList = accessMessageRepository.findAll();
+        List<AccessMessage> accessMessageList = null;
+        if(page == null) {
+            accessMessageList = accessMessageRepository.findAll();
+        } else {
+            accessMessageList = accessMessageRepository.findAllByPage(page);
+        }
         model.put("accessMessageList", accessMessageList);
         if(principal != null) {
             model.put("name", principal.getName());
