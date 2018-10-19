0cc9b3b39f73cf3cca8f0c05ccf3aa67d50e570e
apu <pasha_anik@ukr.net>
2018-10-19 19:56:41 +0300

    'Refactoring

--- src/main/java/com/apu/TcpServerForAccessControlMVC/controller/IndexController.java
+++ src/main/java/com/apu/TcpServerForAccessControlMVC/controller/IndexController.java
@@ -1,38 +1,22 @@
 package com.apu.TcpServerForAccessControlMVC.controller;
 
+import java.security.Principal;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.apu.TcpServerForAccessControlDB.entity.Card;
-import com.apu.TcpServerForAccessControlDB.entity.Device;
-import com.apu.TcpServerForAccessControlMVC.service.CardService;
-import com.apu.TcpServerForAccessControlMVC.service.DeviceService;
-
 @Controller
 public class IndexController {
     
-    @Autowired
-    private CardService cardService;
-    
-    @Autowired
-    private DeviceService deviceService;
-    
     @GetMapping("/")
-    public ModelAndView index() {
+    public ModelAndView index(Principal principal) {
         Map<String, Object> model = new HashMap<>();
-        model.put("name", "Friend");
-        List<Card> cardList = cardService.findAll();
-        model.put("cardList", cardList);
-        List<Device> deviceList = deviceService.findAll();
-        model.put("deviceList", deviceList);
-//        List<AccessMessage> amList = accessMessageService.findAll();
-//        model.put("amList", amList);
+        if(principal != null) {
+            model.put("name", principal.getName());
+        }
         return new ModelAndView("index", model);
     }
 }
