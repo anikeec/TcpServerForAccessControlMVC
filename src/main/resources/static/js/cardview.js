ec36ed2774adae11bb1e76e021b3719f87f607ea
apu <pasha_anik@ukr.net>
2018-10-26 19:40:48 +0300

    Begin add additional JS functionality. Refactoring.

---
+++ src/main/resources/static/js/cardview.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+	var userlist = document.getElementsByClassName("table table-striped table-sm table-hover");
+	var usertable = userlist.item(0).children.item(1);
+	var trlist = usertable.getElementsByTagName("tr");
+	
+})
