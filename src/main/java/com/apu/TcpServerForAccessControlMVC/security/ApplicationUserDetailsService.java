d720d8a087c5cd82be4d539b751502d362c9de4b
apu <pasha_anik@ukr.net>
2018-11-02 17:25:02 +0200

    Fix bug - there not all data were copied. Also user service instead repository

--- src/main/java/com/apu/TcpServerForAccessControlMVC/security/ApplicationUserDetailsService.java
+++ src/main/java/com/apu/TcpServerForAccessControlMVC/security/ApplicationUserDetailsService.java
@@ -1,8 +1,6 @@
 package com.apu.TcpServerForAccessControlMVC.security;
 
-import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -11,46 +9,42 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.apu.TcpServerForAccessControlDB.entity.SystemUser;
-import com.apu.TcpServerForAccessControlDB.entity.UserRole;
-import com.apu.TcpServerForAccessControlDB.repository.SystemUserRepository;
-import com.apu.TcpServerForAccessControlMVC.service.UserRoleService;
 import com.apu.TcpServerForAccessControlMVC.service.i.MvcUserRoleService;
+import com.apu.TcpServerForAccessControlMVC.service.i.MvcUserService;
 
 @Service
 public class ApplicationUserDetailsService implements UserDetailsService {
     
-    private final SystemUserRepository userRepository;
+    private final MvcUserService userService;
     
     private final MvcUserRoleService userRoleService;
     
     @Autowired
-    public ApplicationUserDetailsService(SystemUserRepository userRepository, MvcUserRoleService userRoleService) {
-        this.userRepository = userRepository;
+    public ApplicationUserDetailsService(MvcUserService userService, MvcUserRoleService userRoleService) {
+        this.userService = userService;
         this.userRoleService = userRoleService;
     }
     
     @Override
     public UserDetails loadUserByUsername(String username) {
-        List<SystemUser> userList = userRepository.findByEmail(username);
+        List<SystemUser> userList = userService.findByEmail(username);
         SystemUser user = null;
         if((userList != null)&&(userList.size() > 0)) {
             SystemUser sUser = userList.get(0);
             user = new SystemUser();
-            user.setEmail(sUser.getEmail());
+            user.setUserId(sUser.getUserId());
             user.setFirstName(sUser.getFirstName());
             user.setSecondName(sUser.getSecondName());
-            Set<UserRole> roles = new LinkedHashSet<>();
-            
-            UserRole urAdmin = userRoleService.findUserRoleByDescription("ROLE_ADMIN");
-            UserRole urUser = userRoleService.findUserRoleByDescription("ROLE_USER");
-            
-            roles.add(urAdmin);
-            roles.add(urUser);
-            user.setUserRoleCollection(roles);
+            user.setEmail(sUser.getEmail());            
+            user.setPassword(sUser.getPassword());
+            user.setPhoneNumber(sUser.getPhoneNumber());
+            user.setActive(sUser.getActive());            
+            user.setCardCollection(sUser.getCardCollection());
+            user.setUserRoleCollection(sUser.getUserRoleCollection());
         } else {
             throw new UsernameNotFoundException("User with email %s could not be found - " + username);
         }
-//        User user = userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException(
+//        User user = userService.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException(
 //        String.format("User with email %s could not be found", username)));
         return new ApplicationUserDetails(user);
     }
